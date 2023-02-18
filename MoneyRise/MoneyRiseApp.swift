feee04f8687b2c35f0e5e3df83d897d9dc1e8ee8
AlexanderArefyev <aleksandrarefev99@gmail.com>
2023-02-18 18:27:09 +0300

    Start App

---
+++ MoneyRise/MoneyRiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyRiseApp.swift
+//  MoneyRise
+//
+//  Created by Александр Арефьев on 18.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyRiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
