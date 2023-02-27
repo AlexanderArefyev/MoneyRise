17b56729425b7c8d82119a10d77dc52287238e87
AlexanderArefyev <aleksandrarefev99@gmail.com>
2023-02-27 20:36:55 +0300

    Two buttons

--- MoneyRise/Views/ContentView.swift
+++ MoneyRise/Views/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     
     @State private var balance = 0
     @State var amountNew = ""
-    @State var showAlert = false
     
     var body: some View {
         VStack {
@@ -19,45 +18,71 @@ struct ContentView: View {
                 Text("\(balance)")
                     .font(.system(size: 30, weight: .semibold))
                     .padding(.bottom, 50)
-                Text("Введите и нажмите ОК, чтобы добавить. Для того, чтобы внести расход введите число с минусом.")
+                Text("Введите сумму и выберете нужное дейсвие, чтобы взаимодействовать с Вашим счетом.\nПриятного пользования!")
                     .font(.body)
                     .foregroundColor(.gray)
                     .multilineTextAlignment(.center)
                     .opacity(0.6)
                     .padding(.horizontal, 50)
             }
-            .padding(.bottom, 50)
+            .padding(.bottom, 25)
             .padding(.top, 160)
-            HStack {
+            VStack {
                 TextField("Введите..", text: $amountNew)
                     .textFieldStyle(.roundedBorder)
                     .font(.system(size: 24))
                     .keyboardType(.numberPad)
-                    .padding(.trailing, 15)
-                Button(action: {
-                    balance_update()
-                }, label: {
-                    Text("OK")
-                        .font(.headline)
-                        .frame(width: 40, height: 35)
-                        .foregroundColor(Color.white)
-                        .background(Color.blue)
-                        .cornerRadius(4)
-                        .shadow(color: Color.blue.opacity(0.3), radius: 7, x: 0, y: 3)
-                })
+                GeometryReader { geometry in
+                    HStack (spacing: 17) {
+                        Button(action: {
+                            balance_update_minus()
+                        }, label: {
+                            Text("Расход")
+                                .font(.headline)
+                                .frame(width: geometry.size.width / 2.1, height: 35)
+                                .foregroundColor(Color.white)
+                                .background(Color.red)
+                                .cornerRadius(5)
+                        })
+                        Button(action: {
+                            balance_update_plus()
+                        }, label: {
+                            Text("Доход")
+                                .font(.headline)
+                                .frame(width: geometry.size.width / 2.1, height: 35)
+                                .foregroundColor(Color.white)
+                                .background(Color.green)
+                                .cornerRadius(5)
+                        })
+                    }
+                }
+                .padding(.top, 10)
             }
+            .padding(.top, 20)
             .padding(.horizontal, 40)
             Spacer()
         }
+        
     }
-    func balance_update() {
+    
+    func balance_update_plus() {
         if amountNew.isEmpty {
-            amountNew = "Пусто..."
+            amountNew = ""
         } else {
             balance += Int(amountNew)!
             amountNew = ""
         }
     }
+    
+    func balance_update_minus() {
+        if amountNew.isEmpty {
+            amountNew = ""
+        } else {
+            balance -= Int(amountNew)!
+            amountNew = ""
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
